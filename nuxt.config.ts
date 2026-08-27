c3d59652a35bdc9dcdd517610acfe9e5bc566a90
redouaneT <redouane.taleb1@gmail.com>
2026-08-27 01:45:03 -0400

    chore(dev): run identity background processes locally
    Signed-off-by: redouaneT <redouane.taleb1@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
     langDir: 'locales'
   },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV === 'development' },
   devServer: {
     port: 3100
   },
